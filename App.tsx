7021ae67a82f4c0d4816294936c496d31e555f83
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-01 14:19:03 +0530

    Revert "App renders and asks for permission indicates the pi to show location"
    This reverts commit 8bb790ada5773b1148b824d9ba5002ead83a2cd3.

--- App.tsx
+++ App.tsx
@@ -5,89 +5,22 @@
  * @format
  */
 
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, PermissionsAndroid, Platform } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-//geo location to get a location
-import Geolocation from '@react-native-community/geolocation';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-type LocationType = {
-  latitude: number;
-  longitude: number;
-};
 function App() {
-  const [location, setLocation] = useState<LocationType | null>(null);
-  //request permission on android
-  const requestLocationPermission = async () => {
-    if (Platform.OS === 'android') {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      );
-      return granted == PermissionsAndroid.RESULTS.GRANTED;
-    }
-    return true;
-  };
-
-  // Get current location
-  const getLocation = async () => {
-    const hasPermission = await requestLocationPermission();
-
-    if (!hasPermission) {
-      console.log('Permission denied');
-      return;
-    }
-
-    Geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-        setLocation(position.coords);
-      },
-      error => {
-        console.log(error);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 15000,
-        maximumAge: 10000,
-      },
-    );
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
-
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <MapView
-        style={styles.map}
-        region={
-          location
-            ? {
-                latitude: location.latitude,
-                longitude: location.longitude,
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05,
-              }
-            : {
-                latitude: 22.3039,
-                longitude: 70.8022,
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05,
-              }
-        }
-      >
-        {/* Show marker if location exists */}
-        {location && (
-          <Marker
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-            title="You are here"
-          />
-        )}
-      </MapView>
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 22.3039,
+          longitude: 70.8022,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
@@ -96,7 +29,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  map: { flex: 1 },
 });
 
 export default App;
